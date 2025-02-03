ba69b681e4f92769415b81ff1cd57c8f10548a3d
isaiahpawlak <ipawlak@my.wctc.edu>
2025-02-03 09:39:27 -0600

    Assignment 2 Commit

---
+++ discount.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('discount-row').addEventListener('click', function(e){
+      if (e.target.classList.contains('discount')) {
+        e.preventDefault();
+        document.getElementById('product').innerHTML = e.target.dataset['product'];
+        document.getElementById('code').innerHTML = e.target.dataset['code'];
+        document.getElementById('title').innerHTML = e.target.dataset['title'];
+        bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
+      }
+    });
+  });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var toastEL = document.getElementById('liveToast');
+    var toast = new bootstrap.Toast(toastEL, {
+        autohide: false
+    });
+    toast.show();
+
+    document.addEventListener('keydown', function(e) {
+        if (event.key === 'Escape') {
+            toast.hide();
+        }
+    });
+});
+  
